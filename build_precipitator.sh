4dc8cb9c10dbce2db832de520260bf5917d15ea5
Ben Wibking <ben@wibking.com>
2025-12-10 16:53:31 -0500

    fix build

--- build_precipitator.sh
+++ build_precipitator.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-set -x
-python3 configure.py -openpmd --openpmd_path extern/openPMD-api --prob precipitator --flux hlld --coord spherical_polar -mpi -b
+set -euxo pipefail
+
+OPENPMD_SRC="extern/openPMD-api"
+OPENPMD_BUILD="${OPENPMD_SRC}/build"
+OPENPMD_PREFIX="${OPENPMD_BUILD}/install"
+
+cmake -S "${OPENPMD_SRC}" -B "${OPENPMD_BUILD}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="${OPENPMD_PREFIX}" \
+  -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_TESTING=OFF \
+  -DopenPMD_BUILD_CLI_TOOLS=OFF \
+  -DopenPMD_USE_MPI=ON \
+  -DopenPMD_USE_PYTHON=OFF
+cmake --build "${OPENPMD_BUILD}" --target openPMD -- -j8
+cmake --install "${OPENPMD_BUILD}"
+
+python3 configure.py -openpmd --openpmd_path "${OPENPMD_PREFIX}" --prob precipitator --flux hlld --coord spherical_polar -mpi -b
 make -j8
